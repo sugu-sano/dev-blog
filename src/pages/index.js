336a466ea0e8562fd4862ab723635f03e2933886
sugu-sano <sa.50.00.no.riku+github@gmail.com>
2021-03-07 22:55:27 +0900

    不要なインポートの削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,7 @@
 import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
 import { Redirect } from "@docusaurus/router";
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
   return <Redirect to="/dev-blog/blog" />;
 }
 
